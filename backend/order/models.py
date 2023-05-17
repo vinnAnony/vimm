a6057b757c81885e0c90924358fe42d0d56e0b83
jexi-The Linux <vnjeru85@gmail.com>
2023-05-17 15:23:47 +0300

    remove zipcode from model

--- backend/order/models.py
+++ backend/order/models.py
@@ -11,7 +11,6 @@ class Order(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=100)
     place = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
