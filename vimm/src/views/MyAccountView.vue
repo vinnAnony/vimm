91bd258391755ac7c1f00ac44709993a032e9910
jexi-The Linux <vnjeru85@gmail.com>
2023-05-17 16:01:09 +0300

    display order summary in my account page

--- vimm/src/views/MyAccountView.vue
+++ vimm/src/views/MyAccountView.vue
@@ -8,23 +8,50 @@
             <div class="column is-12">
                 <button @click="logout()" class="button is-danger">Log out</button>
             </div>
+
+            <hr>
+
+            <div class="column is-12">
+                <h2 class="subtitle">My orders</h2>
+
+                <OrderSummary v-for="order in orders" :key="order.id" :order="order" />
+            </div>
         </div>
     </div>
 </template>
 <script>
 import axios from 'axios'
+import OrderSummary from '@/components/OrderSummary.vue'
 
 export default {
     name: "MyAccountView",
+    components: {
+        OrderSummary
+    },
     data() {
         return {
-
+            orders: [],
         }
     },
     mounted() {
         document.title = `Vimm | My Account`
+
+        this.getMyOrders()
     },
     methods: {
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+
+            await axios.get(`/api/v1/orders/`)
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+            this.$store.commit('setIsLoading', false)
+        },
         logout() {
             axios.defaults.headers.common['Authorization'] = ""
 
